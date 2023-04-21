a3c1d4d49943887e262caf176df0a9efc2e15a14
n-saraseka <78618008+n-saraseka@users.noreply.github.com>
2023-04-21 11:32:23 +0300

    More logging

--- main.py
+++ main.py
@@ -1,104 +1,103 @@
-import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import vk_api
 from typing import List
-import os
-import base64
+from chat_worker import chat_worker
+from user_worker import user_worker
 from tokens import group_token, user_token, group_id
-from vk_methods import sender, user_get, group_name_get
-from api_and_stuff import vk_session, vk_user_session, current_dir, Chat, db
+from vk_methods import sender
+from api_and_stuff import vk_session, vk_user_session, current_dir, Chat, Video, db, dbx, del_from_dropbox
 from render_messages import bot_render
 from multiprocessing import Process, Queue
-from peewee import *
 import logging
+import signal
+import time
+import requests
+from vk_api import ApiError
 
 longpoll = VkBotLongPoll(vk_session, group_id)
 logging.basicConfig(filename='info.log', format='%(asctime)s - %(message)s', level=logging.INFO)
 
+def term_handler():
+    unreceiver()
+
+
+def unreceiver():
+    db.connect()
+    query = Chat.select().where(Chat.message_received == True)
+    if len(query):
+        for chat in query:
+            chat.message_received = False
+            chat.save()
+    db.close()
 
 def render_video(msg, id, video_name):
     bot_render(msg, id, video_name)
 
-def notify():
-    while True:
-        query = Chat.get_or_none(Chat.message_received == False and Chat.bot_notifications == True)
-        if query is not None:
-            print(query.message_received)
-            if query.id == 5:
-                sender(query.id, 'Бот запущен! Наслаждайтесь судебными прениями.')
-                query.message_received = True    
-                query.save()
-                print(query.message_received)
-
 def worker(queue, worker_id):
-    while True:
-        task = queue.get()
-        id = task[0]
-        msg = task[1]
-        if list(msg.keys()).count('action')>0: 
-            query = Chat.select().where(Chat.id == id)
-            if (query.exists())==False:
-                chat = Chat.create(id = id, message_received = True, bot_notifications = True)
-                chat.save(force_insert=True)
-            if msg['action']['type']=='chat_invite_user' and msg['action']['member_id']==group_id*(-1):
-                logging.info(f'Bot added to chat. CHAT_ID: {id}')
-                sender(id, 'Добрый день, господа юристы! Я — бот, создающий сцены в стиле Ace Attorney.\nПерешлите мне переписку с командой "@acecourtbotvk суд" и я создам с ней видео.\nПомощь по всем командам можно получить по команде "@acecourtbotvk помощь".\nО всех проблемах, связанных с работой бота, обращайтесь в группу: vk.com/aceCourtBotVK или к создателю vk.com/saraseka.')
-        else:
-            #notifs
-            
-            query = Chat.get_or_none(Chat.id == id)
-            if query==None:
-                print('passed through if statement')
-                chat = Chat.create(id = id, message_received = False, bot_notifications = True)
-                chat.save()
-            
-            if msg['text'].startswith('[club204496105'):
-                if 'суд' in msg['text'].lower() or ('сус' in msg['text'].lower() and 'суд' not in msg['text'].lower()):
-                    messages = vk_session.method('messages.getByConversationMessageId', {'peer_id': msg['peer_id'], 'conversation_message_ids': msg['conversation_message_id'], 'extended': 1})
-                    messages = messages['items'][0]['fwd_messages']
-                    if len(messages)>0 and len(messages)<=100:
-                        logging.info(f'Video is being rendered. CHAT_ID: {id}')
-                        name = base64.b64encode(os.urandom(16)).decode('ascii').replace('/', '').replace('=', '').replace('+', '')
-                        '''
-                        vid = Video.create(id = id, title = name, messages = json.dumps(msg), running = True, finished = False, worker_id = worker_id)
-                        vid.save()
-                        '''
-                        bot_render(msg, id, name)
-                    elif len(messages)>100:
-                        sender(id, 'Вы переслали слишком много сообщений. Пожалуйста, уменьшите количество сообщений и перешлите их ещё раз.')
+    db.connect()
+    try:
+        while True:
+            task = queue.get()
+            id = task[0]
+            msg = task[1]
+            from_chat = task[2]
+            try:
+                del_from_dropbox()
+            except Exception:
+                pass
+            if list(msg.keys()).count('action')>0:
+                query = Chat.select().where(Chat.id == id)
+                if msg['action']['type']=='chat_invite_user' and msg['action']['member_id']==group_id*(-1):
+                    logging.info(f'Bot added to chat. CHAT_ID: {id}')
+                    if (query.exists())==False:
+                        chat = Chat.create(id = id, message_received = True, bot_notifications = True, kicked=False, from_chat = True)
                     else:
-                        sender(id, 'В самом деле, Вы удивили судью, не переслав ни одного сообщения!\nДанный бот принимает только пересланные сообщения; ответы не работают. Если Вы попытались запросить видео ответом на сообщение, то попробуйте его переслать.\nВ ином случае, по всей видимости, Вам захотелось подразнить судью. Не отвлекайте его от работы!')
-                elif 'помощь' in msg['text'].lower():
-                    if '-m' in msg['text'].lower():
-                        sender(id, "PWR — Phoenix Wright: Ace Attorney\nJFA — Phoenix Wright: Ace Attorney — Justice For All\nT&T — Phoenix Wright: Ace Attorney — Trials and Tribulations\nAAI — Ace Attorney Investigations: Miles Edgeworth\nAAI2 — Ace Attorney Investigations: Miles Edgeworth 2\nAJ — Apollo Justice: Ace Attorney\nDD — Phoenix Wright: Ace Attorney — Dual Destinies\nSOJ — Phoenix Wright: Ace Attorney — Spirit of Justice\nRND — случайное аудио, стоит по умолчанию.")
-                    else:
-                        sender(id, "Команды:\nсуд — создание видео.\n-ig — выбор персонажей с игнорированием пола пользователей.\n-m — выбор OST'а по коду. Коды для данной команды вы можете получить по команде '@acecourtbotvk помощь -m'")
-                        logging.info(f'"Help" command executed. CHAT_ID: {id}')
+                        chat = query.get()
+                        chat.kicked = False
+                    chat.save()
+                    sender(id, 'Добрый день, господа юристы! Я — бот, создающий сцены в стиле Ace Attorney.\nПерешлите мне переписку с командой "@AABot_VK суд" и я создам с ней видео.\nПомощь по всем командам можно получить по команде "@acecourtbotvk помощь".\nО всех проблемах, связанных с работой бота, обращайтесь в группу: vk.com/aceCourtBotVK или к создателю vk.com/saraseka.', from_chat)
+            else:
+                if from_chat:
+                    chat_worker(id, msg, from_chat)
+                else:
+                    user_worker(id, msg, from_chat)
+    except Exception as e: 
+        print(f'Worker stopped with exception {e}')
 
 if __name__=='__main__':
+    try:
+        open('info.log', 'w').close()
+        signal.signal(signal.SIGTERM, term_handler)
+        del_from_dropbox()
+        unreceiver()
 
-    query = Chat.select().where(Chat.message_received == True)
-    while query.exists():
-        query = Chat.select().where(Chat.message_received == True)
-        for chat in query:
-            chat.message_received = False
-            chat.save()
-            print(chat.message_received)
-    
-    notificator = Process(target=notify, args=())
-    notificator.start()
-    
-    task_queue = Queue()
-    workers = []
-    workers_amount = 1
-    for i in range(workers_amount):
-        worker_id = i
-        workers.append(Process(target=worker, args=(task_queue, worker_id)))
-        workers[i].start()
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            if event.from_chat:
-                for i in range(workers_amount):
-                    if workers[i].is_alive()==False:
-                        workers[i] = Process(target=worker, args=(task_queue, worker_id))
-                        workers[i].start()
-                task_queue.put([event.chat_id, event.object.message], True, 1/3)
+        task_queue = Queue()
+        workers = []
+        workers_amount = 3
+        for i in range(workers_amount):
+            worker_id = i
+            workers.append(Process(target=worker, args=(task_queue, worker_id)))
+            workers[i].start()
+            print(f'Worker No. {i} has started working')
+        for event in longpoll.listen():
+            try:
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    if event.from_chat:
+                        task_queue.put([event.chat_id, event.object.message, True], True, 1/3)
+                    else:
+                        task_queue.put([event.object.message['from_id'], event.object.message, False], True, 1/3)
+                    for i in range(workers_amount):
+                        if workers[i].is_alive()==False:
+                            print(f'Worker No. {i} has stopped working. Restarting...')
+                            workers[i] = Process(target=worker, args=(task_queue, worker_id))
+                            workers[i].start()
+                time.sleep(1/3)
+            except Exception as e:
+                print(f'Longpoll failed with exception {e}')
+                logging.info(f'Longpoll failed with exception {e}')
+                vk_session = vk_api.VkApi(token = group_token)
+                vk_user_session = vk_api.VkApi(token = user_token)
+                longpoll = VkBotLongPoll(vk_session, group_id)
+    except KeyboardInterrupt:
+        del_from_dropbox()
+        term_handler()
